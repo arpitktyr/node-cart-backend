07f9773c1638223bc5c8a585f4033024492de483
arpitktyr <86348432+arpitktyr@users.noreply.github.com>
2023-04-12 00:06:07 +0530

    some changes done

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const { add, get } = require('../data/user');
-const { createJSONToken, isValidPassword } = require('../util/auth');
-const { isValidEmail, isValidText } = require('../util/validation');
+const express = require("express");
+const { add, get } = require("../data/user");
+const { createJSONToken, isValidPassword } = require("../util/auth");
+const { isValidEmail, isValidText } = require("../util/validation");
 
 const router = express.Router();
 
-router.post('/signup', async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   const data = req.body;
   let errors = {};
 
   if (!isValidEmail(data.email)) {
-    errors.email = 'Invalid email.';
+    errors.email = "Invalid email.";
   } else {
     try {
       const existingUser = await get(data.email);
       if (existingUser) {
-        errors.email = 'Email exists already.';
+        errors.email = "Email exists already.";
       }
     } catch (error) {}
   }
 
   if (!isValidText(data.password, 6)) {
-    errors.password = 'Invalid password. Must be at least 6 characters long.';
+    errors.password = "Invalid password. Must be at least 6 characters long.";
   }
 
   if (Object.keys(errors).length > 0) {
     return res.status(422).json({
-      message: 'User signup failed due to validation errors.',
+      message: "User signup failed due to validation errors.",
       errors,
     });
   }
@@ -36,35 +36,35 @@ router.post('/signup', async (req, res, next) => {
     const authToken = createJSONToken(createdUser.email);
     res
       .status(201)
-      .json({ message: 'User created.', user: createdUser, token: authToken });
+      .json({ message: "User created.", user: createdUser, token: authToken });
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   let user;
   try {
     user = await get(email);
-   // console.log(user);
+    // console.log(user);
   } catch (error) {
-    return res.status(401).json({ message: 'Authentication failed.' });
+    return res.status(401).json({ message: "Authentication failed." });
   }
 
   const pwIsValid = await isValidPassword(password, user.password);
   if (!pwIsValid) {
     return res.status(422).json({
-      message: 'Invalid credentials.',
-      errors: { credentials: 'Invalid email or password entered.' },
+      message: "Invalid credentials.",
+      errors: { credentials: "Invalid email or password entered." },
     });
   }
 
   const token = createJSONToken(email);
-  
-  res.json({ token, id:user.id });
+
+  res.json({ token, id: user.id, email: user.email, name: user.name });
 });
 
 module.exports = router;
