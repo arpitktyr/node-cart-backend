b84a5979b9a2172db241bebe822bf04cfe508f36
arpitktyr <arpitktyr@gmail.com>
2023-06-10 15:13:07 +0530

    catImage

--- models/category.model.js
+++ models/category.model.js
@@ -9,6 +9,9 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  catImage: {
+    type: String,
+  },
 });
 
 const Category = mongoose.model("categories", categorySchema);
